66c5255591e8486ef67ccf2fb99aa1cf6954a24d
Shahzaib Shahbaz <shahzaibshahbaz2002@gmail.com>
2024-04-14 17:57:34 +0500

    New page and a button
    Added a new SecondPage, and a button in the maindart

---
+++ lib/SecondPage.dart
@@ -0,0 +1,17 @@
+// SecondPage.dart
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: Text('This is the second page!'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
+// main.dart
 import 'package:flutter/material.dart';
+
+import 'SecondPage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Text('Weeee x2', style:TextStyle(color:Colors.cyan)),
-              Text('hii'),
-              Text('Aap el gay kyun hain'),
-            ],
-          )
-        ),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+              child: Column(
+                children: [
+                  Text('Weeee x2', style: TextStyle(color: Colors.cyan)),
+                  Text('hii'),
+                  Text('Aap el gay kyun hain'),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    SecondPage()));
+                      },
+                      child: Text("HEYY PRESS MEE!!! :3"))
+                ],
+              )),
+        ));
   }
 }
