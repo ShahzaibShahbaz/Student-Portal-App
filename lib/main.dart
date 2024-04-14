2dfe2db33f476755c158ced7c5047196fade74ce
Shahzaib Shahbaz <shahzaibshahbaz2002@gmail.com>
2024-04-15 01:29:20 +0500

    firebase connection
    added firebase connection using firebase cli. (haven't tested yet).

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'second_page.dart';
 import 'third_page.dart';
 import 'login_page.dart';
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
